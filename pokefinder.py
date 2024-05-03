b51ac682a392382ca7b62a663852aed2e706f46b
Theseus <eli.suligoy@gmail.com>
2024-05-02 21:18:40 -0300

    Agregado el tema de Bootstrap

--- pokefinder.py
+++ pokefinder.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template,request,redirect
+from flask import send_from_directory
 
 app= Flask(__name__,template_folder='templates')
+
+
+@app.route("/css/<archivocss>")
+def css_link(archivocss):
+    return send_from_directory(os.path.join('templates/css/'),archivocss)
 
 
 @app.route('/')
